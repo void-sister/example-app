7a492304fbd54ccb5708fcaede0df755b796a994
varya <el.varvara.el@gmail.com>
2021-08-21 19:34:07 +0300

    cool admin + authentication

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\PlantController;
-use App\Http\Controllers\ShopController;
-use App\Http\Controllers\CartController;
+use App\Http\Controllers\Auth\LoginController;
+//use App\Http\Controllers\CategoryController;
+//use App\Http\Controllers\PlantController;
+//use App\Http\Controllers\ShopController;
+//use App\Http\Controllers\CartController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,25 +19,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/login', [LoginController::class, 'login']);
+Route::post('/login', [LoginController::class, 'authenticate'])->name('authenticate');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::get('/about', function () {
-    return view('about');
-});
 
-Route::get('/plants', [PlantController::class, 'index'])->name('plants.index');
-Route::get('/plants/create', [PlantController::class, 'create']);
-Route::post('/plants', [PlantController::class, 'store'])->name('plants.store');
-Route::get('/plants/{slug}', [PlantController::class, 'show']);
-Route::get('/plants/{slug}/edit', [PlantController::class, 'edit']);
-Route::put('/plants/{slug}', [PlantController::class, 'update'])->name('plants.update');
-Route::post('/plants/{slug}/archive', [PlantController::class, 'archive'])->name('plants.archive');
-Route::post('/plants/{slug}/return', [PlantController::class, 'return'])->name('plants.return');
+
+//
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+//
+//Route::get('/about', function () {
+//    return view('about');
+//});
+
+//Route::get('/plants', [PlantController::class, 'index'])->name('plants.index');
+//Route::get('/plants/create', [PlantController::class, 'create']);
+//Route::post('/plants', [PlantController::class, 'store'])->name('plants.store');
+//Route::get('/plants/{slug}', [PlantController::class, 'show']);
+//Route::get('/plants/{slug}/edit', [PlantController::class, 'edit']);
+//Route::put('/plants/{slug}', [PlantController::class, 'update'])->name('plants.update');
+//Route::post('/plants/{slug}/archive', [PlantController::class, 'archive'])->name('plants.archive');
+//Route::post('/plants/{slug}/return', [PlantController::class, 'return'])->name('plants.return');
 //Route::post('/plants/{slug}/add-to-cart', [PlantController::class, 'addToCart'])->name('plants.add-to-cart');
 
-Route::get('/shop', [ShopController::class, 'index'])->name('shop.index');
+//Route::get('/shop', [ShopController::class, 'index'])->name('shop.index');
 
 //Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
 //Route::put('/cart/update', [CartController::class, 'update'])->name('cart.update');
