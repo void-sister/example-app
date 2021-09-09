c49d160584d3910315227c94178e5d3c7452ba20
varya <el.varvara.el@gmail.com>
2021-09-09 20:02:36 +0300

    edit back btn

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -4,7 +4,7 @@
     <div class="col-lg-12">
         <div class="card">
             <div class="card-header">
-                <a type="button" href="{{ url()->previous() }}" class="btn btn-outline-info mr-2">< Back</a>
+                <a type="button" href="{{ route('users.index') }}" class="btn btn-outline-info mr-2">< Back</a>
                 <strong>Edit User</strong> Form
             </div>
             <div class="card-body card-block">
