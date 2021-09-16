c17a8a263d852e3e9c28ce1b6c912a75433e61dc
varya <el.varvara.el@gmail.com>
2021-09-16 21:56:00 +0300

    index route redirect

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,6 +10,15 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+    public function index()
+    {
+        if (!Auth::check()) {
+            return redirect('login');
+        }
+
+        return redirect()->route('dashboard');
+    }
+
     public function login()
     {
         return view('auth.login');
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', [AuthController::class, 'index']);
+
 Route::get('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/login', [AuthController::class, 'authenticate'])->name('authenticate');
 
