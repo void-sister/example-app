2e9774aaed213a87a16df76fdf9c0cabf34a7eb1
varya <el.varvara.el@gmail.com>
2021-09-17 16:04:10 +0300

    fixed migrations

--- database/migrations/2021_08_04_183523_create_plants_categories_table.php
+++ database/migrations/2021_08_04_183523_create_plants_categories_table.php
@@ -20,7 +20,6 @@ class CreatePlantsCategoriesTable extends Migration
             $table->string('name_uk');
             $table->text('description_ru');
             $table->text('description_uk');
-            $table->timestamps();
         });
     }
 
